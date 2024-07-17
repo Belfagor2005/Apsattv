7c42b6544c8b27c65ac4f223d78dc7a890094f42
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-07-17 17:07:55 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -37,7 +37,7 @@ if [ $PYTHON = "PY3" ]; then
 	if grep -qs "Package: $Packagesix" cat $STATUS ; then
 		echo ""
 	else
-		opkg update && opkg install python3-six
+		opkg update && opkg --force-reinstall --force-overwrite install python3-six
 	fi
 fi
 echo ""
@@ -50,10 +50,10 @@ else
 		apt-get update && apt-get install python-requests -y
 	 
 	elif [ $PYTHON = "PY3" ]; then
-		opkg update && opkg install python3-requests
+		opkg update && opkg --force-reinstall --force-overwrite install python3-requests
 	elif [ $PYTHON = "PY2" ]; then
 	  
-		opkg update && opkg install python-requests
+		opkg update && opkg --force-reinstall --force-overwrite install python-requests
 	
 	fi
 fi
@@ -82,7 +82,7 @@ else
 fi
 
 if [ $OSTYPE != "DreamOs" ]; then
-	opkg update && opkg install ffmpeg gstplayer exteplayer3 enigma2-plugin-systemplugins-serviceapp
+	opkg update && opkg --force-reinstall --force-overwrite install ffmpeg gstplayer exteplayer3 enigma2-plugin-systemplugins-serviceapp
 fi
 sleep 2
 

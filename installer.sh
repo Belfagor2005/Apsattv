6cc3cb98ae5a1279771eed789c269ad936aee1fd
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2024-09-12 12:39:27 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -23,6 +23,18 @@ else
    OSTYPE=Dream
 fi
 echo ""
+
+if [ -f /usr/bin/wget ]; then
+    echo "wget exist"
+else
+	if [ $OSTYPE = "DreamOs" ]; then
+		echo "dreamos"
+		apt-get update && apt-get install wget
+	else
+		opkg update && opkg install wget
+	fi
+fi
+
 if python --version 2>&1 | grep -q '^Python 3\.'; then
 	echo "You have Python3 image"
 	PYTHON=PY3

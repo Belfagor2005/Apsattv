aa3ece05f4e8b2e4f953dcfdcaf4a05021e40884
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-07-12 21:50:10 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,125 +1,101 @@
 #!/bin/bash
 ## setup command=wget -q --no-check-certificate https://raw.githubusercontent.com/Belfagor2005/Apsattv/main/installer.sh -O - | /bin/sh
 
-## Only This 2 lines to edit with new version ######
 version='1.3'
 changelog='\nAdd ChannelUp / ChannelDown in Player\nFix major'
-##############################################################
 TMPPATH=/tmp/Apsattv-main
 FILEPATH=/tmp/main.tar.gz
 
 if [ ! -d /usr/lib64 ]; then
-	PLUGINPATH=/usr/lib/enigma2/python/Plugins/Extensions/Apsattv
+    PLUGINPATH=/usr/lib/enigma2/python/Plugins/Extensions/Apsattv
 else
-	PLUGINPATH=/usr/lib64/enigma2/python/Plugins/Extensions/Apsattv
+    PLUGINPATH=/usr/lib64/enigma2/python/Plugins/Extensions/Apsattv
 fi
 
-## check depends packges
 if [ -f /var/lib/dpkg/status ]; then
-   STATUS=/var/lib/dpkg/status
-   OSTYPE=DreamOs
+    STATUS=/var/lib/dpkg/status
+    OSTYPE=DreamOs
 else
-   STATUS=/var/lib/opkg/status
-   OSTYPE=Dream
+    STATUS=/var/lib/opkg/status
+    OSTYPE=Dream
 fi
 echo ""
 
 if [ -f /usr/bin/wget ]; then
     echo "wget exist"
 else
-	if [ $OSTYPE = "DreamOs" ]; then
-		echo "dreamos"
-		apt-get update && apt-get install wget
-	else
-		opkg update && opkg install wget
-	fi
+    if [ $OSTYPE = "DreamOs" ]; then
+        apt-get update && apt-get install wget
+    else
+        opkg update && opkg install wget
+    fi
 fi
 
 if python --version 2>&1 | grep -q '^Python 3\.'; then
-	echo "You have Python3 image"
-	PYTHON=PY3
-	Packagesix=python3-six
-	Packagerequests=python3-requests
+    PYTHON=PY3
+    Packagesix=python3-six
+    Packagerequests=python3-requests
 else
-	echo "You have Python2 image"
-	PYTHON=PY2
-	Packagerequests=python-requests
+    PYTHON=PY2
+    Packagerequests=python-requests
 fi
 
 if [ $PYTHON = "PY3" ]; then
-	if grep -qs "Package: $Packagesix" cat $STATUS ; then
-		echo ""
-	else
-		opkg update && opkg --force-reinstall --force-overwrite install python3-six
-	fi
+    if grep -qs "Package: $Packagesix" $STATUS ; then
+        echo ""
+    else
+        opkg update && opkg install python3-six
+    fi
 fi
 echo ""
-if grep -qs "Package: $Packagerequests" cat $STATUS ; then
-	echo ""
+if grep -qs "Package: $Packagerequests" $STATUS ; then
+    echo ""
 else
-	echo "Need to install $Packagerequests"
-	echo ""
-	if [ $OSTYPE = "DreamOs" ]; then
-		apt-get update && apt-get install python-requests -y
-	 
-	elif [ $PYTHON = "PY3" ]; then
-		opkg update && opkg --force-reinstall --force-overwrite install python3-requests
-	elif [ $PYTHON = "PY2" ]; then
-	  
-		opkg update && opkg --force-reinstall --force-overwrite install python-requests
-	
-	fi
+    echo "Need to install $Packagerequests"
+    echo ""
+    if [ $OSTYPE = "DreamOs" ]; then
+        apt-get update && apt-get install python-requests -y
+    elif [ $PYTHON = "PY3" ]; then
+        opkg update && opkg install python3-requests
+    elif [ $PYTHON = "PY2" ]; then
+        opkg update && opkg install python-requests
+    fi
 fi
 echo ""
 
-## Remove tmp directory
-## [ -r $TMPPATH ] && rm -f $TMPPATH > /dev/null 2>&1
-
-## Remove tmp directory
-## [ -r $FILEPATH ] && rm -f $FILEPATH > /dev/null 2>&1
-
-## Remove old plugin directory
-## [ -r $PLUGINPATH ] && rm -rf $PLUGINPATH
-
-## Download and install plugin
-## check depends packges
 mkdir -p $TMPPATH
 cd $TMPPATH
 set -e
+
 if [ -f /var/lib/dpkg/status ]; then
-   echo "# Your image is OE2.5/2.6 #"
-   echo ""
+    echo "# Your image is OE2.5/2.6 #"
+    echo ""
 else
-   echo "# Your image is OE2.0 #"
-   echo ""
+    echo "# Your image is OE2.0 #"
+    echo ""
 fi
 
 if [ $OSTYPE != "DreamOs" ]; then
-	opkg update && opkg --force-reinstall --force-overwrite install ffmpeg gstplayer exteplayer3 enigma2-plugin-systemplugins-serviceapp
+    opkg update && opkg install ffmpeg gstplayer exteplayer3 enigma2-plugin-systemplugins-serviceapp
 fi
 sleep 2
 
-wget --no-check-certificate 'https://github.com/Belfagor2005/Apsattv/archive/refs/heads/main.tar.gz'
-tar -xzf main.tar.gz
-cp -r 'Apsattv-main/usr' '/'
+wget --no-check-certificate 'https://github.com/Belfagor2005/Apsattv/archive/refs/heads/main.tar.gz' -O $FILEPATH || { echo "Download failed"; exit 1; }
+tar -xzf $FILEPATH -C /tmp/ || { echo "Extraction failed"; exit 1; }
+cp -r /tmp/Apsattv-main/usr/ / || { echo "Copy failed"; exit 1; }
 set +e
 cd
 sleep 2
 
-## Check if plugin installed correctly
 if [ ! -d $PLUGINPATH ]; then
-	echo "Some thing wrong .. Plugin not installed"
-	rm -rf $TMPPATH > /dev/null 2>&1	
-	exit 1
+    echo "Some thing wrong .. Plugin not installed"
+    rm -rf $TMPPATH > /dev/null 2>&1
+    exit 1
 fi
 rm -rf $TMPPATH > /dev/null 2>&1
+rm -f $FILEPATH > /dev/null 2>&1
 sync
 
-
-exec > >(tee -a /tmp/debug.txt) 2>&1
-set -x
-
-# # Identify the box type from the hostname file
 FILE="/etc/image-version"
 box_type=$(head -n 1 /etc/hostname)
 distro_value=$(grep '^distro=' "$FILE" | awk -F '=' '{print $2}')
